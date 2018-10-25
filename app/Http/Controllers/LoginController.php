05dda0985cb03bb9b20c6c248f20036df9779431
franscev <francisco_javier_sarro_apps1ma1718@cev.com>
2018-10-25 12:37:29 +0200

    login sin token

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Login;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function login($user, $password){
+
+        $userToLogin = ['paco', '1234'];
+
+        if (($userToLogin[0] == $user) && ($userToLogin[1] == $password)){
+            
+            $acces = 'login succesfull';
+           
+            return response()->json([
+
+           $acces
+            
+            ]);
+        }
+
+        else {
+
+            $acces = 'Error 401';
+            
+            return response()->json([
+           
+            $acces
+            
+            ]);
+
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Login  $login
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return response()->json([
+            'id' => $user->name,
+            'title' => $user->password,
+
+            ]);
+           
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Login  $login
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Login $login)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Login  $login
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Login $login)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Login  $login
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Login $login)
+    {
+        //
+    }
+}
